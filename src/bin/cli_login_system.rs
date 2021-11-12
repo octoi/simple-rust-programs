0c056d583b2d15413087afccb7e6b7b9f851032b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-11-12 20:02:22 +0530

    rust progrm

--- src/bin/cli_login_system.rs
+++ src/bin/cli_login_system.rs
@@ -1,5 +1,9 @@
 // Basic login system
 
+use std::io;
+use std::io::Write;
+use std::process;
+
 struct User {
     username: String,
     password: String,
@@ -7,7 +11,7 @@ struct User {
 }
 
 impl User {
-    fn new( username: &str, password: &str, message: &str) -> Self {
+    fn new(username: &str, password: &str, message: &str) -> Self {
         Self {
             username: username.to_string(),
             password: password.to_string(),
@@ -22,4 +26,39 @@ fn main() {
         User::new("go", "gagoo", "Go gang!"),
         User::new("javascript", "nan!=Nan=true", "Js is booootiful !"),
     ];
+
+    let username = input("Enter your username: ");
+    let password = input("Enter your password: ");
+
+    // login
+    for user in users {
+        login(&user, &username.trim(), &password.trim());
+    }
+
+    println!("Seems like there is no user with username {}", username);
+}
+
+fn login(user: &User, username: &str, password: &str) {
+    if user.username == username.to_string() {
+        if user.password == password.to_string() {
+            println!("Welcome back {} 🥳", username);
+            println!("secret message: {}", user.message);
+        } else {
+            println!("Incorrect password for {}", username);
+        }
+
+        process::exit(0);
+    }
+}
+
+
+fn input(message: &str) -> String {
+    let mut input_value = String::new();
+
+    print!("{}", message);
+
+    io::stdout().flush().expect("Failed to take input");
+    io::stdin().read_line(&mut input_value).expect("Failed to read line");
+
+    return input_value;
 }
